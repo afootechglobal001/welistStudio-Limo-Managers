408659ca2bf2c403b3a8f8953900c8f3ddf018d0
Emmytech12 <seunemmanuel107@gmail.com>
2026-04-25 22:24:36 +0100

    feat: create other pages

---
+++ src/features/portal/company-profile/CompanyCard.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import Image from "next/image";
+import { MapPin, Globe, Mail, LucideIcon } from "lucide-react";
+
+type StatItem = {
+  label: string;
+  value: number;
+  icon: LucideIcon;
+};
+
+type CompanyCardProps = {
+  name: string;
+  logo: string;
+  location: string;
+  website: string;
+  email: string;
+  status?: "Active" | "Inactive";
+  stats: StatItem[];
+};
+
+export function CompanyCard({
+  name,
+  logo,
+  location,
+  website,
+  email,
+  status = "Active",
+  stats,
+}: CompanyCardProps) {
+  return (
+    <div className="flex gap-4 p-5 bg-white/2 border border-white/20 rounded-[20px] hover:bg-white/5 hover:border-(--secondary-color-light) transition-all duration-300">
+      <div className="flex w-35 items-center justify-center border rounded-[20px] border-white/20">
+        <Image
+          src={logo}
+          alt={name}
+          className="w-full h-full object-cover rounded-[20px]"
+          width={0}
+          height={0}
+          unoptimized
+        />
+      </div>
+
+      {/* Content */}
+      <div className="flex flex-col gap-4 w-[calc(100%-156px)]">
+        <div className="flex flex-col gap-2">
+          <div className="w-full flex items-center justify-between">
+            <h2 className="flex items-center text-3xl font-bold-custom text-(--title-color)">
+              {name}
+            </h2>
+
+            <div className="flex items-center gap-3">
+              <button className="cursor-pointer border-none bg-linear-to-br from-(--primary-color) to-(--secondary-color) bg-size-[200%_100%] hover:bg-size-[150%_100%] bg-right text-white rounded-full text-[14px] h-8 px-5 flex items-center justify-center duration-200">
+                View
+              </button>
+
+              <span className="px-4 py-2 text-xs font-medium text-(--active-color) bg-green-500/10 border border-green-500/30 rounded-full">
+                {status}
+              </span>
+            </div>
+          </div>
+
+          <div className="flex gap-4 flex-wrap">
+            <div className="flex gap-1 items-center">
+              <MapPin className="h-4 w-4 text-(--secondary-color)" />
+              <span>{location}</span>
+            </div>
+
+            <div className="flex gap-1 items-center">
+              <Globe className="h-4 w-4 text-(--secondary-color)" />
+              <span>{website}</span>
+            </div>
+
+            <div className="flex gap-1 items-center">
+              <Mail className="h-4 w-4 text-(--secondary-color)" />
+              <span>{email}</span>
+            </div>
+          </div>
+        </div>
+
+        {/* Stats */}
+        <div className="flex gap-1 w-full justify-between">
+          {stats.map((item, index) => {
+            const Icon = item.icon;
+
+            return (
+              <div
+                key={index}
+                className="flex px-2.5 py-2 bg-white/2 border border-white/20 rounded-[10px]"
+              >
+                <div className="flex gap-6 items-center justify-between">
+                  <div className="flex gap-2 items-center">
+                    <Icon className="h-6 w-6 p-1 text-(--secondary-color) bg-white/8 border border-white/20 rounded-lg" />
+                    <h4 className="text-[13px]">{item.label}</h4>
+                  </div>
+
+                  <span className="py-1 px-3 font-bold-custom text-(--secondary-color) text-sm bg-white/8 border border-white/20 rounded-lg">
+                    {item.value}
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/portal/company-profile/CompanyProfile.tsx
+++ src/features/portal/company-profile/CompanyProfile.tsx
@@ -3,6 +3,8 @@ import { Building } from "lucide-react";
 import { PageHeader } from "@/features/PageHeader";
 import { PortalWrapper } from "../PortalWrapper";
 import { PageSearch } from "@/components/general-components/pageSearch";
+import { COMPANY_LIST } from "@/constants/portal/companydata";
+import { CompanyCard } from "./CompanyCard";
 
 export default function CompanyProfile() {
   return (
@@ -17,7 +19,7 @@ export default function CompanyProfile() {
         description="Here's an overview of your company profile. You can manage your company information, update contact details, and customize your settings."
         actions={
           <PageSearch
-            placeHolder="Search Here "
+            placeHolder="Search Here"
             actionBtn
             buttonText="Add New Profile"
             onButtonClick={() => {}}
@@ -26,10 +28,11 @@ export default function CompanyProfile() {
       />
 
       <section className="p-8 text-(text-color) flex flex-col gap-8">
-        <p>
-          Manage your company profile, update contact details, and customize
-          your settings.
-        </p>
+        <div className="w-full grid grid-cols-2 gap-5">
+          {COMPANY_LIST.map((company, index) => (
+            <CompanyCard key={index} {...company} />
+          ))}
+        </div>
       </section>
     </PortalWrapper>
   );
